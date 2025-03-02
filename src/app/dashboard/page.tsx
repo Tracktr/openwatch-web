d3375b6af5ef4d00ec8b6fa187119bd45ddb5807
Rowan-Paul <contact@rowanpaulflynn.com>
2025-03-02 16:37:50 +0100

    chore: Add max width to applications

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { DashboardHeader } from '@/components/dashboard-header';
 
 export default async function DashboardPage() {
   return (
-    <div className='space-y-4 p-4 md:p-8 pt-6 min-h-screen'>
+    <div className='space-y-4 p-4 md:p-8 pt-6 min-h-screen max-w-screen-2xl justify-center mx-auto'>
       <DashboardHeader heading="Applications" text="Create and manage your API applications.">
         <CreateApplication />
       </DashboardHeader>
